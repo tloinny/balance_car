87463adae6ec625f2bc1458012e7fa671240237c
tloinny <tloinny@gmail.com>
2019-07-29 20:28:56 +0800

    2019.7.29 20:28 debug

--- src/balance_car/balance_car.ino
+++ src/balance_car/balance_car.ino
@@ -2,25 +2,26 @@
 #include <L298N.h>
 #include <easy_pid.h>
 
-const int ENA = 0;
-const int IN1 = 8;
-const int IN2 = 9;
-const int IN3 = 10;
-const int IN4 = 11;
-const int ENB = 0;	/* only one motor */
+const int ENA = 6;
+const int IN1 = 7;
+const int IN2 = 8;
+const int IN3 = 9;
+const int IN4 = 10;
+const int ENB = 11;	/* only one motor */
 
 L298N DCMotor(ENA,IN1,IN2,IN3,IN4,ENB);	/* motor driver */
 PID_CONTROLLER PID_contorller(2, 0.7, 5, 0.00000001, 0.7);
 MPU6050 mpu6050;
 
 unsigned long timer = 0;
-float timeStep = 0.01;
+float timeStep = 0.008;
 
 float pitch = 0;
 float roll = 0;
 float yaw = 0;
 
 float output = 0;
+int throttle = 0;
 
 void setup()
 {
@@ -31,6 +32,7 @@ void setup()
 	  Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
 	  delay(500);
 	}
+  Serial.println("ok");
 	/* 
 	 * Calibrate gyroscope. The calibration must be at rest.
 	 * If you don't want calibrate, comment this line. 
@@ -40,7 +42,7 @@ void setup()
 	 * Set threshold sensivty. Default 3.
 	 * If you don't want use threshold, comment this line or set 0.
 	 */
-	mpu6050.setThreshold(3);
+	mpu6050.setThreshold(0.1);
 	PID_contorller.setGoal(0);
 }
 
@@ -53,15 +55,26 @@ void loop()
 	pitch = pitch + norm.YAxis * timeStep;
 	/* Wait to full timeStep period */
 	output = PID_contorller.update(pitch);
-	if (output > 0)
+	if (output < 0)
 	{
-		DCMotor.forward(output,0);
-	}else if (output < 0)
+		output = -1 * output;
+		throttle = map(output, 0, 80, 20, 255);
+		DCMotor.forward(throttle,0);
+	}else if (output > 0)
 	{
-		DCMotor.backward(output,0);
+		throttle = map(output, 0, 80, 20, 255);
+		DCMotor.backward(throttle,0);
 	}else
 	{
 		DCMotor.full_stop(0);
 	}
+ Serial.print("YAxis:");
+ Serial.print(norm.YAxis,DEC);
+ Serial.print(" ");
+	Serial.print("pitch:");
+	Serial.print(pitch,DEC);
+	Serial.print(" ");
+	Serial.print("throttle:");
+	Serial.println(throttle,DEC);
 	delay((timeStep*1000) - (millis() - timer));
 }
